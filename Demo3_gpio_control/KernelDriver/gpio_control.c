f6f0911f664cd4f7704ecc4d25588ccea43c4247
zyx <2511305125@qq.com>
2025-07-20 01:24:35 +0100

    Refactor GPIO control logic in driver: validate GPIO, handle request and direction setting errors

--- Demo3_gpio_control/KernelDriver/gpio_control.c
+++ Demo3_gpio_control/KernelDriver/gpio_control.c
@@ -15,12 +15,39 @@ static int __init simple_led_init(void)
     printk(KERN_INFO "My simple LED test, initing....\n");
     printk(KERN_INFO "Attempting to contril GPIO %d\n",SYS_LED_GPIO);
 
+    if(!gpio_is_valid(SYS_LED_GPIO))
+    {
+        printk(KERN_ERR "gpio %d is not valid on this system",SYS_LED_GPIO);
+        return -ENODEV;
+    }
+
+    ret = gpio_request(SYS_LED_GPIO,"my_led");
+    if(ret)
+    {
+        printk(KERN_ERR "Failed to request GPIO %d",SYS_LED_GPIO);
+        return ret;
+    }
+
+    ret = gpio_direction_output(SYS_LED_GPIO,1);
+    if(ret)
+    {
+        printk(KERN_ERR "Failed to set GPIO direction");
+        gpio_free(SYS_LED_GPIO);
+        return ret;
+    }
+
+    printk(KERN_INFO "Successfully took control of GPIO %d, LED should be ON\n",SYS_LED_GPIO);
     return 0;
 }
 
 static void __exit simple_led_exit(void)
 {
+    printk(KERN_INFO "My simple led demo, exitint.....\n");
 
+    gpio_set_value(SYS_LED_GPIO,0);
+    gpio_free(SYS_LED_GPIO);
+
+    printk(KERN_INFO "GPIO %d released\n",SYS_LED_GPIO);
 }
 
 module_init(simple_led_init);
